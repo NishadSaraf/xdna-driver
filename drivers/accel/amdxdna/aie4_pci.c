NishadSaraf/xdna-driver

commit 0a5063ac30647627410eb08cda2fe933d89d15d5

accel/amdxdna: Add basic support for AIE4 devices
Add initial support for AIE4 devices (PCI device IDs 0x17F2 and 0x1B0B),
including:
  Device initialization
  Basic mailbox communication
  SR-IOV enablement

This lays the groundwork for full AIE4 support.

Co-developed-by: Hayden Laccabue <Hayden.Laccabue@amd.com>
Signed-off-by: Hayden Laccabue <Hayden.Laccabue@amd.com>
Signed-off-by: David Zhang <yidong.zhang@amd.com>

drivers/accel/amdxdna/aie4_pci.c
@@ -1,0 +1,364 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Copyright (C) 2026, Advanced Micro Devices, Inc.
+ */
+
+#include "drm/amdxdna_accel.h"
+#include <drm/drm_managed.h>
+#include <drm/drm_print.h>
+
+#include "aie4_pci.h"
+#include "amdxdna_pci_drv.h"
+
+#define NO_IOHUB	0
+
+/*
+ * The management mailbox channel is allocated by firmware.
+ * The related register and ring buffer information is on SRAM BAR.
+ * This struct is the register layout.
+ */
+struct mailbox_info {
+	__u32 valid;
+	__u32 protocol_major;
+	__u32 protocol_minor;
+	__u32 x2i_tail_offset;
+	__u32 x2i_head_offset;
+	__u32 x2i_buffer_addr;
+	__u32 x2i_buffer_size;
+	__u32 i2x_tail_offset;
+	__u32 i2x_head_offset;
+	__u32 i2x_buffer_addr;
+	__u32 i2x_buffer_size;
+	__u32 i2x_msi_idx;
+	__u32 reserved[4];
+};
+
+static int aie4_fw_is_alive(struct amdxdna_dev *xdna)
+{
+	const struct amdxdna_dev_priv *npriv = xdna->dev_info->dev_priv;
+	struct amdxdna_dev_hdl *ndev = xdna->dev_handle;
+	u32 __iomem *src;
+	u32 fw_is_valid;
+	int ret;
+
+	src = ndev->rbuf_base + npriv->mbox_info_off;
+
+	ret = readx_poll_timeout(readl, src + offsetof(struct mailbox_info, valid),
+				 fw_is_valid, (fw_is_valid == 0x1),
+				 AIE_INTERVAL, AIE_TIMEOUT);
+	if (ret)
+		XDNA_ERR(xdna, "fw_is_valid=%d after %d ms",
+			 fw_is_valid, DIV_ROUND_CLOSEST(AIE_TIMEOUT, 1000000));
+
+	return ret;
+}
+
+static void aie4_read_mbox_info(struct amdxdna_dev *xdna,
+				struct mailbox_info *mbox_info)
+{
+	const struct amdxdna_dev_priv *npriv = xdna->dev_info->dev_priv;
+	struct amdxdna_dev_hdl *ndev = xdna->dev_handle;
+	u32 *dst = (u32 *)mbox_info;
+	u32 __iomem *src;
+	int i;
+
+	src = ndev->rbuf_base + npriv->mbox_info_off;
+
+	for (i = 0; i < sizeof(*mbox_info) / sizeof(u32); i++)
+		dst[i] = readl(&src[i]);
+}
+
+static int aie4_mailbox_info(struct amdxdna_dev *xdna,
+			     struct mailbox_info *mbox_info)
+{
+	int ret;
+
+	ret = aie4_fw_is_alive(xdna);
+	if (ret)
+		return ret;
+
+	aie4_read_mbox_info(xdna, mbox_info);
+
+	ret = aie_check_protocol(&xdna->dev_handle->aie,
+				 mbox_info->protocol_major,
+				 mbox_info->protocol_minor);
+	if (ret)
+		XDNA_ERR(xdna, "mailbox major.minor %d.%d is not supported",
+			 mbox_info->protocol_major, mbox_info->protocol_minor);
+
+	return ret;
+}
+
+static void aie4_mailbox_fini(struct amdxdna_dev_hdl *ndev)
+{
+	struct amdxdna_dev *xdna = ndev->aie.xdna;
+
+	aie_destroy_chann(&ndev->aie, &ndev->aie.mgmt_chann);
+	drmm_kfree(&xdna->ddev, ndev->mbox);
+	ndev->mbox = NULL;
+}
+
+static int aie4_irq_init(struct amdxdna_dev *xdna)
+{
+	struct pci_dev *pdev = to_pci_dev(xdna->ddev.dev);
+	int ret, nvec;
+
+	nvec = pci_msix_vec_count(pdev);
+	XDNA_DBG(xdna, "irq vectors:%d", nvec);
+	if (nvec <= 0) {
+		XDNA_ERR(xdna, "does not get number of interrupt vector");
+		return -EINVAL;
+	}
+
+	ret = pci_alloc_irq_vectors(pdev, nvec, nvec, PCI_IRQ_MSIX);
+	if (ret < 0) {
+		XDNA_ERR(xdna, "failed to alloc irq vector, ret: %d", ret);
+		return ret;
+	}
+
+	return 0;
+}
+
+static int aie4_mailbox_start(struct amdxdna_dev *xdna,
+			      struct mailbox_info *mbi)
+{
+	struct pci_dev *pdev = to_pci_dev(xdna->ddev.dev);
+	struct amdxdna_dev_hdl *ndev = xdna->dev_handle;
+	const struct amdxdna_dev_priv *npriv = xdna->dev_info->dev_priv;
+	struct xdna_mailbox_chann_res *i2x;
+	struct xdna_mailbox_chann_res *x2i;
+	int mgmt_mb_irq;
+	int ret;
+
+	struct xdna_mailbox_res mbox_res = {
+		.ringbuf_base = ndev->rbuf_base,
+		.ringbuf_size = pci_resource_len(pdev, npriv->mbox_rbuf_bar),
+		.mbox_base = ndev->mbox_base,
+		.mbox_size = pci_resource_len(pdev, npriv->mbox_bar),
+		.name = "xdna_aie4_mailbox",
+	};
+
+	i2x = &ndev->aie.mgmt_i2x;
+	x2i = &ndev->aie.mgmt_x2i;
+
+	x2i->mb_head_ptr_reg = mbi->x2i_head_offset;
+	x2i->mb_tail_ptr_reg = mbi->x2i_tail_offset;
+	x2i->rb_start_addr = mbi->x2i_buffer_addr;
+	x2i->rb_size = mbi->x2i_buffer_size;
+
+	i2x->rb_start_addr = mbi->i2x_buffer_addr;
+	i2x->rb_size = mbi->i2x_buffer_size;
+	i2x->mb_head_ptr_reg = mbi->i2x_head_offset;
+	i2x->mb_tail_ptr_reg = mbi->i2x_tail_offset;
+
+	ndev->aie.mgmt_chan_idx = mbi->i2x_msi_idx;
+	aie_dump_mgmt_chann_debug(&ndev->aie);
+
+	ndev->mbox = xdnam_mailbox_create(&xdna->ddev, &mbox_res);
+	if (!ndev->mbox) {
+		XDNA_ERR(xdna, "failed to create mailbox device");
+		return -ENODEV;
+	}
+
+	ndev->aie.mgmt_chann = xdna_mailbox_alloc_channel(ndev->mbox);
+	if (!ndev->aie.mgmt_chann) {
+		XDNA_ERR(xdna, "failed to alloc mailbox channel");
+		return -ENODEV;
+	}
+
+	mgmt_mb_irq = pci_irq_vector(pdev, ndev->aie.mgmt_chan_idx);
+	if (mgmt_mb_irq < 0) {
+		XDNA_ERR(xdna, "failed to alloc irq vector, return %d", mgmt_mb_irq);
+		ret = mgmt_mb_irq;
+		goto free_channel;
+	}
+
+	ret = xdna_mailbox_start_channel(ndev->aie.mgmt_chann,
+					 &ndev->aie.mgmt_x2i,
+					 &ndev->aie.mgmt_i2x,
+					 NO_IOHUB,
+					 mgmt_mb_irq);
+	if (ret) {
+		XDNA_ERR(xdna, "failed to start management mailbox channel");
+		ret = -EINVAL;
+		goto free_channel;
+	}
+
+	XDNA_DBG(xdna, "Mailbox management channel created");
+	return 0;
+
+free_channel:
+	xdna_mailbox_free_channel(ndev->aie.mgmt_chann);
+	ndev->aie.mgmt_chann = NULL;
+	return ret;
+}
+
+static int aie4_mailbox_init(struct amdxdna_dev *xdna)
+{
+	struct mailbox_info mbox_info;
+	int ret;
+
+	ret = aie4_mailbox_info(xdna, &mbox_info);
+	if (ret)
+		return ret;
+
+	return aie4_mailbox_start(xdna, &mbox_info);
+}
+
+static void aie4_fw_unload(struct amdxdna_dev_hdl *ndev)
+{
+	/* TODO */
+}
+
+static int aie4_fw_load(struct amdxdna_dev_hdl *ndev)
+{
+	/* TODO */
+	return 0;
+}
+
+static int aie4_hw_start(struct amdxdna_dev *xdna)
+{
+	struct amdxdna_dev_hdl *ndev = xdna->dev_handle;
+	int ret;
+
+	ret = aie4_fw_load(ndev);
+	if (ret)
+		return ret;
+
+	ret = aie4_mailbox_init(xdna);
+	if (ret)
+		goto fw_unload;
+
+	return 0;
+
+fw_unload:
+	aie4_fw_unload(ndev);
+
+	return ret;
+}
+
+static void aie4_mgmt_fw_fini(struct amdxdna_dev_hdl *ndev)
+{
+	int ret;
+
+	/* No paired resume needed, fw is stateless */
+	ret = aie4_suspend_fw(ndev);
+	if (ret)
+		XDNA_ERR(ndev->aie.xdna, "suspend_fw failed, ret %d", ret);
+	else
+		XDNA_DBG(ndev->aie.xdna, "npu firmware suspended");
+}
+
+static void aie4_hw_stop(struct amdxdna_dev *xdna)
+{
+	struct amdxdna_dev_hdl *ndev = xdna->dev_handle;
+
+	drm_WARN_ON(&xdna->ddev, !mutex_is_locked(&xdna->dev_lock));
+
+	aie4_mgmt_fw_fini(ndev);
+	aie4_mailbox_fini(ndev);
+
+	aie4_fw_unload(ndev);
+}
+
+static int aie4_pcidev_init(struct amdxdna_dev_hdl *ndev)
+{
+	struct amdxdna_dev *xdna = ndev->aie.xdna;
+	struct pci_dev *pdev = to_pci_dev(xdna->ddev.dev);
+	void __iomem *tbl[PCI_NUM_RESOURCES] = {0};
+	unsigned long bars = 0;
+	int ret, i;
+
+	/* Enable managed PCI device */
+	ret = pcim_enable_device(pdev);
+	if (ret) {
+		XDNA_ERR(xdna, "pcim enable device failed, ret %d", ret);
+		return ret;
+	}
+
+	ret = dma_set_mask_and_coherent(&pdev->dev, DMA_BIT_MASK(64));
+	if (ret) {
+		XDNA_ERR(xdna, "failed to set DMA mask to 64:%d", ret);
+		return ret;
+	}
+
+	set_bit(xdna->dev_info->mbox_bar, &bars);
+	set_bit(xdna->dev_info->sram_bar, &bars);
+
+	for (i = 0; i < PCI_NUM_RESOURCES; i++) {
+		if (!test_bit(i, &bars))
+			continue;
+		tbl[i] = pcim_iomap(pdev, i, 0);
+		if (!tbl[i]) {
+			XDNA_ERR(xdna, "map bar %d failed", i);
+			return -ENOMEM;
+		}
+	}
+
+	ndev->mbox_base = tbl[xdna->dev_info->mbox_bar];
+	ndev->rbuf_base = tbl[xdna->dev_info->sram_bar];
+
+	pci_set_master(pdev);
+
+	ret = aie4_irq_init(xdna);
+	if (ret)
+		goto clear_master;
+
+	ret = aie4_hw_start(xdna);
+	if (ret)
+		goto clear_master;
+
+	return 0;
+
+clear_master:
+	pci_clear_master(pdev);
+
+	return ret;
+}
+
+static void aie4_pcidev_fini(struct amdxdna_dev_hdl *ndev)
+{
+	struct amdxdna_dev *xdna = ndev->aie.xdna;
+	struct pci_dev *pdev = to_pci_dev(xdna->ddev.dev);
+
+	aie4_hw_stop(xdna);
+
+	pci_clear_master(pdev);
+}
+
+static void aie4_fini(struct amdxdna_dev *xdna)
+{
+	struct amdxdna_dev_hdl *ndev = xdna->dev_handle;
+
+	aie4_sriov_stop(ndev);
+	aie4_pcidev_fini(ndev);
+}
+
+static int aie4_init(struct amdxdna_dev *xdna)
+{
+	struct amdxdna_dev_hdl *ndev;
+	int ret;
+
+	ndev = drmm_kzalloc(&xdna->ddev, sizeof(*ndev), GFP_KERNEL);
+	if (!ndev)
+		return -ENOMEM;
+
+	ndev->priv = xdna->dev_info->dev_priv;
+	ndev->aie.xdna = xdna;
+	xdna->dev_handle = ndev;
+
+	ret = aie4_pcidev_init(ndev);
+	if (ret) {
+		XDNA_ERR(xdna, "Setup PCI device failed, ret %d", ret);
+		return ret;
+	}
+
+	XDNA_DBG(xdna, "aie4 init finished");
+	return 0;
+}
+
+const struct amdxdna_dev_ops aie4_ops = {
+	.init			= aie4_init,
+	.fini			= aie4_fini,
+	.sriov_configure        = aie4_sriov_configure,
+};

drivers/accel/amdxdna/Kbuild
@@ -43,7 +43,13 @@ amdxdna-$(OFT_CONFIG_AMDXDNA_PCI) += \
 	aie2_psp.o \
 	aie2_smu.o \
 	aie2_solver.o \
+	aie4_message.o \
+	aie4_pci.o \
 	npu1_regs.o \
+	npu3_regs.o \
 	npu4_regs.o \
 	npu5_regs.o \
-	npu6_regs.o \
+	npu6_regs.o
+
+amdxdna-$(CONFIG_PCI_IOV) += \
+	aie4_sriov.o

drivers/accel/amdxdna/aie.h
@@ -8,6 +8,9 @@
 #include "amdxdna_pci_drv.h"
 #include "amdxdna_mailbox.h"
 
+#define AIE_INTERVAL	20000	/* us */
+#define AIE_TIMEOUT	1000000	/* us */
+
 struct aie_device {
 	struct amdxdna_dev *xdna;
 	struct mailbox_channel *mgmt_chann;

drivers/accel/amdxdna/aie2_pci.c
@@ -80,7 +80,7 @@ static int aie2_get_mgmt_chann_info(struct amdxdna_dev_hdl *ndev)
 	 * is alive.
 	 */
 	ret = readx_poll_timeout(readl, SRAM_GET_ADDR(ndev, FW_ALIVE_OFF),
-				 addr, addr, AIE2_INTERVAL, AIE2_TIMEOUT);
+				 addr, addr, AIE_INTERVAL, AIE_TIMEOUT);
 	if (ret || !addr)
 		return -ETIME;
 

drivers/accel/amdxdna/aie2_pci.h
@@ -14,9 +14,6 @@
 #include "aie2_msg_priv.h"
 #include "amdxdna_mailbox.h"
 
-#define AIE2_INTERVAL	20000	/* us */
-#define AIE2_TIMEOUT	1000000	/* us */
-
 /* Firmware determines device memory base address and size */
 #define AIE2_DEVM_BASE	0x4000000
 #define AIE2_DEVM_SIZE	SZ_64M

drivers/accel/amdxdna/aie2_smu.c
@@ -44,7 +44,7 @@ static int aie2_smu_exec(struct amdxdna_dev_hdl *ndev, u32 reg_cmd,
 	writel(1, SMU_REG(ndev, SMU_INTR_REG));
 
 	ret = readx_poll_timeout(readl, SMU_REG(ndev, SMU_RESP_REG), resp,
-				 resp, AIE2_INTERVAL, AIE2_TIMEOUT);
+				 resp, AIE_INTERVAL, AIE_TIMEOUT);
 	if (ret) {
 		XDNA_ERR(ndev->aie.xdna, "smu cmd %d timed out", reg_cmd);
 		return ret;

drivers/accel/amdxdna/aie4_message.c
@@ -1,0 +1,27 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Copyright (C) 2026, Advanced Micro Devices, Inc.
+ */
+
+#include "drm/amdxdna_accel.h"
+#include <drm/drm_print.h>
+#include <linux/mutex.h>
+
+#include "aie.h"
+#include "aie4_msg_priv.h"
+#include "aie4_pci.h"
+#include "amdxdna_mailbox.h"
+#include "amdxdna_mailbox_helper.h"
+#include "amdxdna_pci_drv.h"
+
+int aie4_suspend_fw(struct amdxdna_dev_hdl *ndev)
+{
+	DECLARE_AIE_MSG(aie4_msg_suspend, AIE4_MSG_OP_SUSPEND);
+	int ret;
+
+	ret = aie_send_mgmt_msg_wait(&ndev->aie, &msg);
+	if (ret)
+		XDNA_ERR(ndev->aie.xdna, "Failed to suspend fw, ret %d", ret);
+
+	return ret;
+}

drivers/accel/amdxdna/aie4_msg_priv.h
@@ -1,0 +1,49 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (C) 2026, Advanced Micro Devices, Inc.
+ */
+
+#ifndef _AIE4_MSG_PRIV_H_
+#define _AIE4_MSG_PRIV_H_
+
+#include <linux/types.h>
+
+enum aie4_msg_opcode {
+	AIE4_MSG_OP_SUSPEND                          = 0x10003,
+
+	AIE4_MSG_OP_CREATE_VFS                       = 0x20001,
+	AIE4_MSG_OP_DESTROY_VFS                      = 0x20002,
+};
+
+enum aie4_msg_status {
+	AIE4_MSG_STATUS_SUCCESS = 0x0,
+	AIE4_MSG_STATUS_ERROR = 0x1,
+	AIE4_MSG_STATUS_NOTSUPP = 0x2,
+	MAX_AIE4_MSG_STATUS_CODE = 0x4,
+};
+
+struct aie4_msg_suspend_req {
+	__u32 rsvd;
+} __packed;
+
+struct aie4_msg_suspend_resp {
+	enum aie4_msg_status status;
+} __packed;
+
+struct aie4_msg_create_vfs_req {
+	__u32 vf_cnt;
+} __packed;
+
+struct aie4_msg_create_vfs_resp {
+	enum aie4_msg_status status;
+} __packed;
+
+struct aie4_msg_destroy_vfs_req {
+	__u32 rsvd;
+} __packed;
+
+struct aie4_msg_destroy_vfs_resp {
+	enum aie4_msg_status status;
+} __packed;
+
+#endif /* _AIE4_MSG_PRIV_H_ */

commit a3955d7ecb8ca03f195a1f590800ea5d31365726

accel/amdxdna: refuse a second aie4_mailbox_init() on a live mailbox
aie4_mailbox_start() publishes ndev->aie.mgmt_chann before it knows the
channel can be started. It asks for the management MSI-X vector with
request_irq(..., 0, ...), so a second request on a line the previous
channel still owns is rejected with -EBUSY: __setup_irq() requires
IRQF_SHARED on both the old and the new action, and the flags here are
plain 0. The rejection is by design, but it arrives too late. The
pointer to the live channel has already been overwritten, and the
free_channel unwind frees the *new* channel and NULLs mgmt_chann, so
the previous one is left allocated, still holding the interrupt, still
running its workqueue, and reachable from nothing. free_irq() takes the
channel as its dev_id, so that interrupt can never be released.

The damage is not confined to the operation that hit it.
aie4_pf_hw_restart() returns the error through its one path that does
no cleanup, and every later aie4_mailbox_init() -- from resume, from
reset_done, from probe -- meets the same -EBUSY against the same
orphan. aie4_mailbox_fini() cannot clear it either: it NULL-checks
mgmt_chann, which the unwind already set to NULL. The device stays
offline for the life of the module.

Check for a live channel and refuse before anything is written.

On reachability, plainly: the routes in are narrow. A duplicate init
needs a caller that skipped aie4_mailbox_fini(), and today every caller
pairs correctly. The two ways to get there are a runtime PM resume
racing an FLR window, which requires a reset trigger holding no runtime
PM reference -- the sysfs reset attribute and AER recovery both take
one -- and a reset_done that restarts the hardware while the PM core
still believes the device is suspended. Neither is likely. What argues
for the guard is that "no live mailbox on entry" is an unwritten and
unenforced precondition shared by six call sites, on paths carrying
"//FIX this after fw_load can work after FLR" annotations that promise
rework. A rework that adds a retry, or calls hw_restart() without a
preceding hw_stop(), earns an unrecoverable device and an -EINVAL with
nothing pointing at the cause. The drm_WARN_ON() names the caller that
broke the contract.

Return -EEXIST rather than -EBUSY. None of the six call sites inspect
the value; they all propagate and abort. The difference is what the
outer consumers do. rpm_callback() treats -EBUSY and -EAGAIN as
transient and does not record power.runtime_error, so a resume failing
that way stays silently retryable against a mailbox that will never
come free. -EEXIST is recorded, surfaces as power/runtime_status
"error", and says what is true: the mailbox is already there.

Only the channel is checked, not ndev->mbox. An aie4_mailbox_init()
that fails after xdnam_mailbox_create() leaves ndev->mbox set, because
those callers return without an aie4_mailbox_fini(); refusing on it as
well would convert a retryable failure into exactly the permanent one
this patch exists to prevent. Overwriting ndev->mbox leaks a drmm
allocation until device release, which is a separate and much smaller
problem.

Taking a runtime PM reference across reset_prepare/reset_done was
considered instead. It only covers the FLR routes, and the obvious form
of it deadlocks: pm_runtime_get_sync() inside amdxdna_reset_prepare()
reenters dev_lock through amdxdna_pm_runtime_resume() while
amdxdna_reset_prepare() already holds it.

aie2_hw_start() assigns ndev->aie.mgmt_chann with the same shape and is
left alone here.

Not exercised on hardware: an FLR on the only board that reaches
aie4_pf_ops takes the host down before reset_done runs, so the path
this guards cannot be reached there. Build tested only.

Fixes: 0a5063ac3064 ("accel/amdxdna: Add basic support for AIE4 devices")

drivers/accel/amdxdna/aie4_pci.c
@@ -363,6 +363,23 @@ static int aie4_mailbox_init(struct amdxdna_dev_hdl *ndev)
 	struct mailbox_info mbox_info;
 	int ret;
 
+	/*
+	 * Callers must tear the mailbox down with aie4_mailbox_fini()
+	 * before starting it again. A live channel owns its MSI-X
+	 * interrupt and is the dev_id free_irq() needs to release it, but
+	 * aie4_mailbox_start() overwrites ndev->aie.mgmt_chann before
+	 * request_irq() gets the chance to reject the duplicate, which
+	 * would strand the previous channel holding an interrupt nothing
+	 * can reach. Refuse while everything can still be undone.
+	 *
+	 * Only the channel is checked. ndev->mbox outlives an init that
+	 * failed after xdnam_mailbox_create(), because those callers
+	 * return without an aie4_mailbox_fini(); keying off it as well
+	 * would turn a retryable failure into a permanent one.
+	 */
+	if (drm_WARN_ON(&xdna->ddev, ndev->aie.mgmt_chann))
+		return -EEXIST;
+
 	ret = aie4_mailbox_info(xdna, &mbox_info);
 	if (ret)
 		return ret;